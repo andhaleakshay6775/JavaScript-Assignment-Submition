131247f0b63ac0bed6397385f0c7e4c95eea9afa
andhaleakshay6775 <68527044+andhaleakshay6775@users.noreply.github.com>
2021-02-11 22:20:44 +0530

    assignment  sol1

---
+++ assignment1/first.js
@@ -0,0 +1,8 @@
+
+
+console.log(1 + "2" + "2");
+console.log(1 + +"2" + "2");
+console.log(1 + -"1" + "2");
+console.log(+"1" + "1" + "2");
+console.log( "A" - "B" + "2");
+console.log( "A" - "B" + 2);
